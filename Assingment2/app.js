8c83f4a75411fe82e52753acca72156e1e30acfa
ahsananees21 <ahsananees21216@gmail.com>
2026-04-15 18:49:59 +0500

    pending

---
+++ Assingment2/app.js
@@ -0,0 +1,30 @@
+// question # 01
+
+let userName;
+
+// question # 02
+
+let myName = ("Ahsan Anees")
+
+// question # 03
+// part a
+let message;
+
+// part b
+message = "Hello World"
+
+// part c
+alert(message);
+
+// question # 04
+let studentName = "Jhone Doe"
+let studentAge = 15
+let studentCourse = "Certified Mobile Application Development"
+
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+
+// question # 05
+let 
+
