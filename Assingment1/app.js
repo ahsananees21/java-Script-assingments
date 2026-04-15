862bccd9b8e1a7e40eaca10ffac03fc2b1b7da17
ahsananees21 <ahsananees21216@gmail.com>
2026-04-15 18:27:48 +0500

    pop up's

---
+++ Assingment1/app.js
@@ -0,0 +1,10 @@
+
+// alert("Hello... I can run JS through my web browser's console")
+
+// alert("Welcome to JS Land")
+
+// alert("Welcome to JS Land... \n Happy Coding!")
+
+// alert("Error! please enter a valid password")
+
+// alert("Welcome my Website")
