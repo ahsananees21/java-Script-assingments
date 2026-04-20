451ee3e743f37e872b826a8f1bedd53b23a1c093
ahsananees21 <ahsananees21216@gmail.com>
2026-04-20 18:55:18 +0500

    Assingment4pending

---
+++ Assingment4/app.js
@@ -0,0 +1,19 @@
+// let studentName , studentRollNo, studentAddress;
+
+
+// illegal
+
+// let student Name; 
+// let 326studentName;
+// let -studentname;
+// let let
+
+// legal
+
+// let studentName;
+// let studentName124;
+// let _studentName;
+// let $studentName;
+// let student325496Name
+
+document.write("<h1>Rules for naming JS variables</h1>")
