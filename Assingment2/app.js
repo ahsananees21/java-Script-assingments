c8aa292040ffd127c1b93d74b37c6ed39c2c9825
ahsananees21 <ahsananees21216@gmail.com>
2026-04-20 17:50:28 +0500

    assingment2

--- Assingment2/app.js
+++ Assingment2/app.js
@@ -1,30 +1,51 @@
-// question # 01
+// // question # 01
 
-let userName;
+// let userName;
 
-// question # 02
+// // question # 02
 
-let myName = ("Ahsan Anees")
+// let myName = ("Ahsan Anees")
 
-// question # 03
-// part a
-let message;
+// // question # 03
+// // part a
+// let message;
 
-// part b
-message = "Hello World"
+// // part b
+// message = "Hello World"
 
-// part c
-alert(message);
+// // part c
+// alert(message);
 
-// question # 04
-let studentName = "Jhone Doe"
-let studentAge = 15
-let studentCourse = "Certified Mobile Application Development"
+// // question # 04
 
-alert(studentName);
-alert(studentAge);
-alert(studentCourse);
+// let studentName = "Jhone Doe"
+// let studentAge = 15
+// let studentCourse = "Certified Mobile Application Development"
 
-// question # 05
-let 
+// alert(studentName);
+// alert(studentAge);
+// alert(studentCourse);
 
+// // question # 05
+
+// alert("PIZZA \nPIZZ \nPIZ \nPI \nP")
+
+// // question # 06
+
+// let email = ("ahsananees21216@gmail.com")
+// alert(email)
+
+// // question # 07
+
+// let book = "A smarter way to learn JavaScript"
+// alert(book)
+
+// question # 08
+
+// let script = "Yah! I can write HTML content through JavaScript"
+// document.write(script)
+
+// question # 09
+
+// let design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(design)
